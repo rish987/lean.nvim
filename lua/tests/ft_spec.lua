227652b26d873995dfdd3e59963a329e60c31138
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-06-12 16:37:03 -0700

    Implement filetype detection unit test for Lean 4.

--- lua/tests/ft_spec.lua
+++ lua/tests/ft_spec.lua
@@ -1,9 +1,15 @@
 describe('filetype detection', function()
   require('tests.helpers').setup {}
 
-  it('recognizes a lean file',
+  it('recognizes a lean 3 file',
     function(_)
-      vim.api.nvim_command("edit test/lean/test.lean")
+      vim.api.nvim_command("edit test/lean3/test.lean")
+      assert.is.same(vim.bo.ft, "lean3")
+    end)
+
+  it('recognizes a lean 4 file',
+    function(_)
+      vim.api.nvim_command("edit test/lean/Test.lean")
       assert.is.same(vim.bo.ft, "lean")
     end)
 end)
