ac5d69a08c584aa7ea7568c31ea532597d3c8919
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-06-13 14:04:48 -0700

    Fix lean3 filetype test and start on lean4 test.

--- lua/tests/ft_spec.lua
+++ lua/tests/ft_spec.lua
@@ -1,9 +1,17 @@
 describe('filetype detection', function()
   require('tests.helpers').setup {}
 
-  it('recognizes a lean file',
+  it('recognizes a lean 3 file',
     function(_)
       vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
-      assert.is.same(vim.bo.ft, "lean")
+      assert.is.same(vim.bo.ft, "lean3")
     end)
+
+  -- NOTE: currently broken (works when manually testsing - timing issue with :setfiletype?)
+
+  --it('recognizes a lean 4 file',
+  --  function(_)
+  --    vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test.lean")
+  --    assert.is.same(vim.bo.ft, "lean")
+  --  end)
 end)
