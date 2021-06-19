2c5e9d3467af164d4aae526007e5b60c215812f5
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-06-18 20:15:20 -0700

    Wait version of LSP tests.

--- lua/tests/lsp/lsp/lsp_spec.lua
+++ lua/tests/lsp/lsp/lsp_spec.lua
@@ -1,34 +1,26 @@
 local helpers = require('tests.helpers')
 
-local function lsp_no_error()
-  local diagnostics = vim.lsp.diagnostic.get()
-
-  assert.is_equal(0, #diagnostics)
-end
-
-local function lsp_test(pos, method, conversion_func, expected)
+local function try_lsp_req(pos, method)
   vim.api.nvim_win_set_cursor(0, pos)
   local params = vim.lsp.util.make_position_params()
 
-  local buf_result = false
+  local req_result
+  local success, _ = vim.wait(10000, function()
+    local results = vim.lsp.buf_request_sync(0, method, params)
+    if results[1] and results[1] == nil then return false end
 
-  vim.lsp.buf_request(0, method, params, function(_, _, result)
-    --print(vim.inspect(result))
-    assert.is_true(type(result) == "table")
-    local out = conversion_func(result)
-    for _, string in pairs(expected) do
-      assert.message( "\nexpected to contain: \n" .. string ..  "\nactual: \n" .. out
-      ).is_truthy(out:find(string, nil, true))
+    for _, result in pairs(results) do
+      req_result = result.result
     end
-    buf_result = true
-  end)
+    if req_result then return true end
 
-  local success, _ = vim.wait(10000, function() return buf_result end)
+    return false
+  end, 1000)
 
-  assert.is_true(success)
+  return success and req_result
 end
 
-describe('lsp', function()
+describe('basic lsp', function()
   helpers.setup {
     lsp = { enable = true },
     lsp3 = { enable = true },
@@ -36,59 +28,54 @@ describe('lsp', function()
   it('lean 3', function()
     vim.api.nvim_command("edit lua/tests/fixtures/example-lean3-project/test.lean")
     helpers.wait_for_ready_lsp()
-    vim.wait(5000)
-
-    lsp_no_error()
+    vim.wait(6000)
 
     it('hover', function()
-      lsp_test({3, 20}, "textDocument/hover",
-      function(result)
-        assert.is_truthy(result.contents)
-        local lines = {}
-        for _, contents in ipairs(result.contents) do
-          if contents.language == 'lean' then
-            vim.list_extend(lines, {contents.value})
-          end
+      local result = try_lsp_req({3, 20}, "textDocument/hover")
+      assert.message("hover request failed").is_truthy(result)
+      assert.is_not_nil(result.contents)
+      assert.are_equal(type(result.contents), "table")
+      local lines = {}
+      for _, contents in ipairs(result.contents) do
+        if contents.language == 'lean' then
+          vim.list_extend(lines, {contents.value})
         end
-        return table.concat(lines, "\n")
-      end,
-      {"test : ℕ"})
+      end
+      local text = table.concat(lines, "\n")
+      assert.has_all(text, {"test : ℕ"})
     end)
     it('definition', function()
-      lsp_test({3, 20}, "textDocument/definition",
-      function(result)
-        assert.is_truthy(result[1])
-        return result[1].uri
-      end,
-      {"tests/fixtures/example-lean3-project/test/test1.lean"})
+      local result = try_lsp_req({3, 20}, "textDocument/definition")
+      assert.message("definition request failed").is_truthy(result)
+      assert.is_not_nil(result[1])
+      assert.is_not_nil(result[1].uri)
+      local text = result[1].uri
+      assert.has_all(text, {"tests/fixtures/example-lean3-project/test/test1.lean"})
     end)
   end)
 
   it('lean 4', function()
     vim.api.nvim_command("edit lua/tests/fixtures/example-lean4-project/Test.lean")
     helpers.wait_for_ready_lsp()
-    vim.wait(5000)
-
-    lsp_no_error()
+    vim.wait(6000)
 
     it('hover', function()
-      lsp_test({3, 20}, "textDocument/hover",
-      function(result)
-        assert.is_truthy(result.contents)
-        return result.contents.value
-      end,
-      {"test : Nat"})
+      local result = try_lsp_req({3, 20}, "textDocument/hover")
+      assert.message("hover request failed").is_truthy(result)
+      assert.is_not_nil(result.contents)
+      assert.is_not_nil(result.contents.value)
+      local text = result.contents.value
+      assert.has_all(text, {"test : Nat"})
     end)
 
     it('definition', function()
-      lsp_test({3, 20}, "textDocument/definition",
-      function(result)
-        assert.is_truthy(result[1])
-        return result[1].targetUri:lower()
-      end,
+      local result = try_lsp_req({3, 20}, "textDocument/definition")
+      assert.message("definition request failed").is_truthy(result)
+      assert.is_not_nil(result[1])
+      assert.is_not_nil(result[1].targetUri)
+      local text = result[1].targetUri:lower()
       -- case-insensitive because MacOS FS is case-insensitive
-      {("tests/fixtures/example-lean4-project/Test/Test1.lean"):lower()})
+      assert.has_all(text, {("tests/fixtures/example-lean4-project/Test/Test1.lean"):lower()})
     end)
   end)
-
 end)
